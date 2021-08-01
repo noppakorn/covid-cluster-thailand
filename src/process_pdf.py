fcb4ee92dcfbdfdd6a32347452835748e8587015
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-01 23:59:40 +0700

    Unified snake_case field for json

--- src/process_pdf.py
+++ src/process_pdf.py
@@ -48,22 +48,22 @@ def extract_cluster(pdf_path : str, pages : set) -> pd.DataFrame:
             df = df.append(table).reset_index(drop=True)
 
     df = df.rename(columns={
-        0 : "PROVINCE_TH",
-        1 : "DISTRICT_TH",
-        2 : "PlaceOfOutbreak",
-        3 : "StartDate",
-        4 : "NewCases",
-        5 : "Total"
+        0 : "province_th",
+        1 : "district_th",
+        2 : "place_of_outbreak",
+        3 : "start_date",
+        4 : "new_cases",
+        5 : "total"
     })
     df = df.replace(r'^\s*$', np.nan, regex=True)
-    df["PROVINCE_TH"] = df["PROVINCE_TH"].ffill()
-    df["Total"] = df["Total"].ffill()
+    df["province_th"] = df["province_th"].ffill()
+    df["total"] = df["total"].ffill()
     df = df.dropna().reset_index(drop=True)
-    df["Total"] = df["Total"].str.split(expand=True)[0]
-    df_startdate = df["StartDate"].str.split(expand=True)
+    df["total"] = df["total"].str.split(expand=True)[0]
+    df_startdate = df["start_date"].str.split(expand=True)
     df_startdate[1] = df_startdate[1].replace(THAIMONTH_TO_MONTH)
     df_startdate[2] = "2021"
-    df["StartDate"] = df_startdate[2] + "-" + df_startdate[1] + "-" + df_startdate[0].str.zfill(2)
+    df["start_date"] = df_startdate[2] + "-" + df_startdate[1] + "-" + df_startdate[0].str.zfill(2)
 
     # Replace newline in cell with space
     regex_newline = re.compile(r"\n")
@@ -71,32 +71,32 @@ def extract_cluster(pdf_path : str, pages : set) -> pd.DataFrame:
 
     # Correction for "ำ" cannot be read properly from PDF
     regex_aum = re.compile(r" า")
-    df["PlaceOfOutbreak"].replace(regex_aum, "ำ", inplace=True)
-    df["DISTRICT_TH"].replace(regex_aum, "ำ", inplace=True)
+    df["place_of_outbreak"].replace(regex_aum, "ำ", inplace=True)
+    df["district_th"].replace(regex_aum, "ำ", inplace=True)
 
     # Province name correction (Will cover "ำ" case as well)
     province_th = PROVINCE_TH_TO_EN.keys()
-    df_invalid_province = df[(~df["PROVINCE_TH"].isin(province_th))]
+    df_invalid_province = df[(~df["province_th"].isin(province_th))]
     if len(df_invalid_province) > 0 :
         # Replace by finding most similar province
-        df_invalid_correted = df_invalid_province["PROVINCE_TH"].apply(lambda pro : find_similar_word(pro, province_th))
+        df_invalid_correted = df_invalid_province["province_th"].apply(lambda pro : find_similar_word(pro, province_th))
         df.update(df_invalid_correted)
         print("Uncorrectable Province:")
-        print(df[(~df["PROVINCE_TH"].isin(province_th))])
+        print(df[(~df["province_th"].isin(province_th))])
 
-    df["PROVINCE_EN"] = df["PROVINCE_TH"].map(PROVINCE_TH_TO_EN)
-    df["PROVINCE_EN"] = df["PROVINCE_EN"].where(pd.notnull(df["PROVINCE_EN"]), None)
+    df["province_en"] = df["province_th"].map(PROVINCE_TH_TO_EN)
+    df["province_en"] = df["province_en"].where(pd.notnull(df["province_en"]), None)
 
     # District name correction (Just in case)
 
-    df["DISTRICT_TH"] = df.apply(lambda row : district_correction(row["DISTRICT_TH"], row["PROVINCE_TH"], PROVINCE_TO_DISTRICT), axis=1)
+    df["district_th"] = df.apply(lambda row : district_correction(row["district_th"], row["province_th"], PROVINCE_TO_DISTRICT), axis=1)
     # Add district in english
-    df["DISTRICT_EN"] = df.apply(lambda row : district_th_to_en(row["DISTRICT_TH"], row["PROVINCE_TH"], PROVINCE_TO_DISTRICT), axis=1)
+    df["district_en"] = df.apply(lambda row : district_th_to_en(row["district_th"], row["province_th"], PROVINCE_TO_DISTRICT), axis=1)
 
     regex_comma = re.compile(r",")
-    df[["NewCases", "Total"]] = df[["NewCases", "Total"]].replace(regex_comma,"").astype(int)
+    df[["new_cases", "total"]] = df[["new_cases", "total"]].replace(regex_comma,"").astype(int)
 
-    return df[["PROVINCE_TH", "PROVINCE_EN", "DISTRICT_TH", "DISTRICT_EN", "PlaceOfOutbreak", "StartDate", "NewCases", "Total"]]
+    return df[["province_th", "province_en", "district_th", "district_en", "place_of_outbreak", "start_date", "new_cases", "total"]]
 
 def extract_cluster_at_date(dto : datetime.datetime) -> bool :
     file_name = f"{str(dto.day).zfill(2)}{str(dto.month).zfill(2)}{str(dto.year+543)[-2:]}.pdf"
@@ -112,8 +112,8 @@ def extract_cluster_at_date(dto : datetime.datetime) -> bool :
             df = extract_cluster(pdf_path, cluster_page)
             if not os.path.exists("../json") : os.mkdir("../json")
             json_dict = {
-                "UpdatedDate": str(dto.date()),
-                "ClusterData": df.to_dict(orient="records")
+                "updated_date": str(dto.date()),
+                "cluster_data": df.to_dict(orient="records")
             }
             with open(f"../json/cluster-data-{bdday_to_date(file_name)}.json", "w+", encoding="utf-8") as json_file :
                 json.dump(json_dict, json_file, ensure_ascii=False, indent=2)
