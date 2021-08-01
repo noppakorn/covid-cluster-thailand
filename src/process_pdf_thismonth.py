670a59d41db24b0e71be785920b72ce9b161ca58
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-02 00:12:48 +0700

    Implement proper month range

--- src/process_pdf_thismonth.py
+++ src/process_pdf_thismonth.py
@@ -2,5 +2,6 @@ from process_pdf import *
 
 if __name__ == "__main__":
     now = datetime.datetime.now()
-    date_range = [now - datetime.timedelta(days=x) for x in range(1,now.day+1)]
-    for date in date_range: extract_cluster_at_date(date)
+    date_range = pd.date_range(start=now.replace(day=1), end=now)
+    for date in date_range: 
+        extract_cluster_at_date(date)
